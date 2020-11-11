e40d67d01de83450a69de0b7ab65f6dc4301cff1
Volodymyr Kovalskyi <vkova002@gold.ac.uk>
2020-11-11 19:08:04 +0000

    Init Upload

---
+++ Menu.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export class Menu extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+// e stands for event! I extract the value from the input menu 
+handleClick(e) {
+  const text = e.target.value; 
+  this.props.chooseVideo(text);
+}
+
+  render() {
+    return (
+      <form onClick={this.handleClick}>
+        <input type="radio" name="src" value="fast" /> fast
+        <input type="radio" name="src" value="slow" /> slow
+        <input type="radio" name="src" value="cute" /> cute
+        <input type="radio" name="src" value="eek" /> eek
+      </form>
+    );
+  }
+}
